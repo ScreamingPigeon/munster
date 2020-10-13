a45dbc96f97f652cdb612f57b6c93a8afd58f4b1
ScreamingPigeon <visitprakhar@gmail.com>
2020-10-13 20:17:41 +0530

    1.001

--- munbase/settings.py
+++ munbase/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'kb5)j&qx0v^=3!=f4845_lskpeaq+mk4bs##*q4uqp2lb-07q9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.233.23.198"]
 
 
 # Application definition
