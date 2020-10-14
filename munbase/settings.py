5f6ff60a6287e9b24eb47108a21572e370872569
ScreamingPigeon <visitprakhar@gmail.com>
2020-10-14 18:41:24 +0530

    1.006

--- munbase/settings.py
+++ munbase/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kb5)j&qx0v^=3!=f4845_lskpeaq+mk4bs##*q4uqp2lb-07q9'
 
 # SECURITYWARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['13.233.23.198']
 
