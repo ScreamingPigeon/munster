9f1c09c505d1e43ee6698644d96c13f52ba326e9
ScreamingPigeon <visitprakhar@gmail.com>
2020-10-15 18:01:50 +0530

    1.008

--- munbase/settings.py
+++ munbase/settings.py
@@ -19,9 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # SECURITYWARNING: keep the secret key used in production secret!
 SECRET_KEY = 'kb5)j&qx0v^=3!=f4845_lskpeaq+mk4bs##*q4uqp2lb-07q9'
 
