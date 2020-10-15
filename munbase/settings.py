1584a91664395dd742f1f2e0e92199752df3fb6d
ScreamingPigeon <visitprakhar@gmail.com>
2020-10-15 19:49:54 +0530

    1.008

--- munbase/settings.py
+++ munbase/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = "/opt/bitnami/projects/PROJECT/PROJECT/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # SECURITYWARNING: keep the secret key used in production secret!
