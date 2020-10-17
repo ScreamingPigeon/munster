0af728dd69ddd1c60a83077f9ea11785c91ba1f0
ScreamingPigeon <visitprakhar@gmail.com>
2020-10-17 16:38:14 +0530

    1.009

--- munbase/urls.py
+++ munbase/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static # to import static in deployment
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
