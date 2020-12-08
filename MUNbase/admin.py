ceeea096f0b1172f3564e33c1edc1f844bf81101
Prakhar <visitprakhar@gmail.com>
2020-12-08 19:54:16 +0530

    edit funct

--- MUNbase/admin.py
+++ MUNbase/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import User, Experience
 admin.site.register(User)
-admin.site.register(User,Experience)
+admin.site.register(Experience)
