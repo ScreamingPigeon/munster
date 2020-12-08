ee9acc8511e5c2371b167bba74e7f7d262822cf0
Prakhar <visitprakhar@gmail.com>
2020-12-08 19:00:21 +0530

    edit funct

--- MUNbase/views.py
+++ MUNbase/views.py
@@ -114,7 +114,7 @@ def editexp(request,MUN, year):
     if request.method == "GET":
         return render(request, "exp/edit.html",{"user":user,"exp":expelement})
     if request.method == "POST":
-
+        expelement=Experience.objects.filter(MUN=MUN, year = int(year))[0]
         mun= request.POST["MUN"]
         year=request.POST["year"]
         committee=request.POST["comm"]
