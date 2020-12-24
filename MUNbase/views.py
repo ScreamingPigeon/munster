b1a1685b60e2c77c47ff027226757be1cf5b3a64
Prakhar <visitprakhar@gmail.com>
2020-12-24 14:35:49 +0530

    24.2

--- MUNbase/views.py
+++ MUNbase/views.py
@@ -210,7 +210,7 @@ def editannouncements(request, heading, content):
             return redirect(reverse('login', errmsg ='You need to login first'))
         elif getusertype(request) != 'MUN':
             return redirect(reverse('settings', errmsg = "That resource cannot be utilized by your account!" ))
-        announcement = MUNannouncements.objects.filter(announcer = getuser(request), heading=heading, content=content)
+        announcement = MUNannouncements.objects.filter(announcer = getuser(request), heading=heading, content=content)[0]
         announcement.heading = request.POST['heading']
         announcement.content = request.POST['content']
         announcement.save()
