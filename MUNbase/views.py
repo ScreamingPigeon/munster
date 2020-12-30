a503cc898a7d6dc724196ffb563a1849d83314b7
Prakhar <visitprakhar@gmail.com>
2020-12-30 20:57:52 +0530

    30

--- MUNbase/views.py
+++ MUNbase/views.py
@@ -427,7 +427,7 @@ def excelr(request):
         worksheet.write(i+1,3, str(registrations[i].delegate.email))
         worksheet.write(i+1,4, str(registrations[i].delegate.city))
         worksheet.write(i+1,5, str(expstring(exp[i]))
-    workbook.close()
+    #workbook.close()
     return str(path)
 #-----------------------------------------ERROR HANDLERS-----------------------------------#
 def error_404_view(request,exception):
