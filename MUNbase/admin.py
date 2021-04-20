cc244d36a00358a7d71e696f21db9bb9a1567101
ScreamingPigeon <visitprakhar@gmail.com>
2021-04-20 22:10:22 +0530

    Scalable Blog?

--- MUNbase/admin.py
+++ MUNbase/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Experience, MUNuser, MUNannouncements, Registrations
+from .models import User, Experience, MUNuser, MUNannouncements, Registrations, Article
 admin.site.register(User)
 admin.site.register(Experience)
 admin.site.register(MUNuser)
 admin.site.register(MUNannouncements)
 admin.site.register(Registrations)
+admin.site.register(Article)
