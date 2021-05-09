bdca7a7c843f8c9b3ac33193856d9ef429714111
ScreamingPigeon <visitprakhar@gmail.com>
2021-05-09 18:47:31 +0530

    autoblog

--- MUNbase/admin.py
+++ MUNbase/admin.py
@@ -7,4 +7,14 @@ admin.site.register(Experience)
 admin.site.register(MUNuser)
 admin.site.register(MUNannouncements)
 admin.site.register(Registrations)
-admin.site.register(Article)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'active', 'author', 'date')
+
+    def active(self, obj):
+        return obj.is_active == 1
+
+    active.boolean = True
+
+admin.site.register(State, StateAdmin)
