738af8432f3f37af9851320dc465f48dca217f54
ScreamingPigeon <visitprakhar@gmail.com>
2021-07-07 11:27:05 +0530

    admin

--- MUNbase/views.py
+++ MUNbase/views.py
@@ -521,13 +521,13 @@ def logincomm(request, munname):
 
 
 def adminview(request, munname, commname):
-    if request.session.get('emun') is not None or request.session.get('emunalloc') is not None or request.session.get('emuncomm') is not None:
-        if request.session['emun'] is not munname or request.session['emunalloc'] is not "Admin" or request.session['emuncomm'] is not commname:
-            del request.session["emun"]
-            del request.session["emunalloc"]
-            del request.session["emuncomm"]
-            url = "http://www.munster.co.in/emun/"+munname
-            return redirect(url)
+    if request.session.get('emun') is not munname or request.session.get('emunalloc') is not "Admin" or request.session.get('emuncomm') is not commname:
+        #if request.session['emun'] is not munname or request.session['emunalloc'] is not "Admin" or request.session['emuncomm'] is not commname:
+        #    del request.session["emun"]
+        #    del request.session["emunalloc"]
+        #    del request.session["emuncomm"]
+        url = "http://www.munster.co.in/emun/"+munname
+        return redirect(url)
     munnamme = request.session.get('emun')
     admin = request.session.get('emunalloc')
     commname = request.session.get('emuncomm')
