8a3d02d3cf6769cea52810e7bc33c0b8a03c5513
ScreamingPigeon <visitprakhar@gmail.com>
2021-07-11 11:50:55 +0530

    discussions

--- MUNbase/views.py
+++ MUNbase/views.py
@@ -723,7 +723,7 @@ def getdiscussions(request, munname, commname):
             talklists += row
         ret = {'talklist': talklists,"talkers":talkers}
         return JsonResponse({"result": ret})
-def getactivediscussion(response, munname, commname):
+def getactivediscussion(request, munname, commname):
     if request.is_ajax and request.method == "GET":
         if request.session.get('emunalloc') is None or request.session.get('emuncomm') is None or request.session.get('emun') is None:
             return None
